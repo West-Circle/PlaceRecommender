a85bc1b4377747f09c8deb0dafc66963e550f4c2
West-Circle <yuan_0006@hotmail.com>
2022-11-18 23:53:01 +0800

    Upload app.py

---
+++ app.py
@@ -0,0 +1,145 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import random 
+import streamlit.components.v1 as stc
+from threading import Thread
+from streamlit.runtime.scriptrunner import add_script_run_ctx
+from streamlit.runtime.scriptrunner.script_run_context import get_script_run_ctx
+
+@st.cache(allow_output_mutation=True)
+def load_data(data):
+    df = pd.read_csv(data, nrows=3500,na_filter=False)
+    return df 
+
+@st.cache(allow_output_mutation=True)
+def getCategory(category):
+    k = [""]
+    for item in category:
+        if not item in k:
+            k.append(item)
+    return k
+
+@st.cache(allow_output_mutation=True)
+def getState(state):
+    k = [""]
+    for item in state:
+        if not item in k:
+            k.append(item)
+    return k
+
+@st.cache(allow_output_mutation=True)
+def getRecommend(search_term, category, state, rate1, rate2, df):
+    df["Rating"] = pd.to_numeric(df["Rating"])
+    arr = []
+    results = df[(df.Category == category) & (df.State == state) & (df.Rating >= rate1) & (df.Rating <= rate2) & (df.Place_Name.str.contains(search_term))] 
+    recommend = results
+    return recommend
+
+#state, place_name, rating, description, website, location_link, address, phone_number, category, picture_link
+htmlResult = '''
+<div style="width:90%;height:100%;margin:1px;padding:5px;position:relative;border-radius:5px;border-bottom-right-radius: 60px;
+box-shadow:0 0 15px 5px #ccc; background-color: #a8f0c6;
+  border-left: 5px solid #6c6c6c;">
+
+<p style="color:blue;"><span style="color:black;">&#127760;State:</span>{}</p>
+<p style="color:blue;"><span style="color:black;">&#127961;Place Name:</span>{}</p>
+<p style="color:blue;"><span style="color:black;">&#127775;Rating:</span>{}</p>
+<p style="color:blue;"><span style="color:black;">&#128462;Description:</span>{}</p>
+<p style="color:blue;"><span style="color:black;">🔗</span><a href="{}",target="_blank">Website</a></p>
+<p style="color:blue;"><span style="color:black;">🔗</span><a href="{}",target="_blank">Location Link</a></p>
+<p style="color:blue;"><span style="color:black;">&#127968;Address:</span>{}</p>
+<p style="color:blue;"><span style="color:black;">&#128222;Phone Number:</span>{}</p>
+<p style="color:blue;"><span style="color:black;">Category:</span>{}</p>
+<p style="color:blue;"><span style="color:black;"></span><img src="{}" alt="Place Image" width="300" height="300"></a></p>
+</div>
+'''
+
+#@st.experimental_singleton(suppress_st_warning=True)
+#@st.cache(allow_output_mutation=True, suppress_st_warning=True) 
+#def main():
+st.title("Place Recommender")
+menu = ["Home","Recommend","About"]
+choice = st.sidebar.selectbox("Menu",menu)
+df = load_data("Malaysia Tourist Attraction.csv")
+df.columns = [c.replace(' ', '_') for c in df.columns]
+if choice == "Home":
+    st.subheader("Home")
+    st.write("Welcome to Place Recommender Page")
+elif choice == "Recommend":
+    category = st.sidebar.selectbox("Category",getCategory(df["Category"]))
+    state = st.sidebar.selectbox("State",getState(df["State"]))
+    rating = ["4.0 - 5.0", "3.0 - 4.0", "2.0 - 3.0", "1.0 - 2.0", "0.0 - 1.0"]
+    rate = st.sidebar.radio("Rating", rating)
+    rate1 = rate2 = 0
+    if(rate == "4.0 - 5.0"):
+        rate1 = 4.0
+        rate2 = 5.0
+    elif rate == "3.0 - 4.0":
+        rate1 = 3.0
+        rate2 = 4.0
+    elif rate == "2.0 - 3.0":
+        rate1 = 2.0
+        rate2 = 3.0
+    elif rate == "1.0 - 2.0":
+        rate1 = 1.0
+        rate2 = 2.0
+    elif rate == "0.0 - 1.0":
+        rate1 = 0.0
+        rate2 = 1.0
+    st.subheader("Recommend Places")
+    search_term = st.text_input("Search")
+    if(st.button("Search")):
+        if category == "":
+            st.error('Please select category', icon="🚨")
+        elif state == "":
+            st.error('Please select state', icon="🚨")
+        if not search_term == "":
+            #try:
+                results = getRecommend(search_term, category, state, rate1, rate2, df)
+                print(len(results))
+                if len(results) == 0:
+                    results = "Not Found"
+                    st.warning(results)
+                else:
+                    st.subheader("Results")
+                    print("Results:")
+                    print(results)
+                    if(len(results) > 5):
+                        results = results.sample(n=5)
+                    print("Recommend 3 Places")
+                    print(results)
+                    for row in results.iterrows():
+                        state  = row[1][0]
+                        place_name = row[1][1]
+                        rating = row[1][2]
+                        description = row[1][3]
+                        website = row[1][4]
+                        location_link = row[1][5]
+                        address = row[1][6]
+                        phone_number = row[1][7]
+                        category = row[1][8]
+                        picture_link = row[1][9]
+                        stc.html(htmlResult.format(state, place_name, rating, description, website, location_link, address, phone_number, category, picture_link),height=750)
+
+            #except:
+            #    results = "Not Found"
+            #    st.warning(results)
+        else:
+            st.error('Please enter search term', icon="🚨")
+    #st.dataframe(df.head(3500))
+else:
+    st.subheader("About")
+    st.text("Built with Streamlit & Pandas")
+
+
+#if __name__ == '__main__':
+    #thread = threading.Thread(target=main)
+    #main()
+    #ctx = get_script_run_ctx()
+    #add_script_run_ctx(ctx)
+    #t = Thread(target=main)
+    #ctx = get_script_run_ctx()
+    #add_script_run_ctx(t, ctx)
+    #t.start()
+    #print(t)
